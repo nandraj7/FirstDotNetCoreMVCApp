eb045ad6acf8aac422ec1a8a02e6dfce89e02975
hp <hp@HPELITEBOOK840G>
2023-03-24 18:20:08 +0530

    Add project files.

---
+++ Models/MvcdemoContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstDotNetCoreMVCApp.Models;
+
+public partial class MvcdemoContext : DbContext
+{
+    public MvcdemoContext()
+    {
+    }
+
+    public MvcdemoContext(DbContextOptions<MvcdemoContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("name=DefaultCS");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC07C0D83377");
+
+            entity.Property(e => e.Gender)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstDotNetCoreMVCApp.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Gender { get; set; }
+}
